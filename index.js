38fce48126e8b4beaa9aeb20313666b54de4739c
fiveace-Merill <freddie.ogutu@gmail.com>
2024-10-24 15:11:19 +0300

    time-stamp-final

--- index.js
+++ index.js
@@ -24,9 +24,20 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", (req, res) => {
+  let dateString  = req.params.date
+  if(!isNaN(dateString)){
+    date = new Date(parseInt(dateString))
+    res.json({ unix : dateString, utc: date.toUTCString()})
+  }else{
+    date = new Date(dateString)
+    res.json({ unix : Math.floor(date.getTime()), utc: date.toUTCString()})
+  }
+})
+
 
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3001, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
